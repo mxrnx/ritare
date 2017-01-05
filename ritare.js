898d4e8a5877768d9b88e1798e788b603c82b8f7
daniilS <daniilS@users.noreply.github.com>
2017-01-05 17:01:28 +0100

    add mobile support, for some reason

--- ritare.js
+++ ritare.js
@@ -1,4 +1,4 @@
-function getoffset(el) {
+function getoffset(el){
 	if(!el){
 		el = this;
 	}
@@ -14,6 +14,113 @@ function getoffset(el) {
 	return [x, y];
 }
 
+
+
+function start(e){
+	offsets = getoffset(Ritare.canvas);
+	if(e.type == 'mousedown'){
+		Ritare.mouseX = e.pageX - offsets[0] - 2;
+		Ritare.mouseY = e.pageY - offsets[1] - 2;
+		Ritare.touch = false;
+	}
+	else if(e.type == 'touchstart'){
+		Ritare.mouseX = e.changedTouches[0].pageX - offsets[0] - 2;
+		Ritare.mouseY = e.changedTouches[0].pageY - offsets[1] - 2;
+		Ritare.touch = true;
+	}
+	if(Ritare.bucketfill && e.type == 'mousedown'){
+		//create a copy of the image's current state for modification
+		var canvas = Ritare.context.getImageData(0, 0, Ritare.canvas.width, Ritare.canvas.height);
+		//create an array of pixels to scan, and a 2d array to see if a pixel has already been scanned
+		var scan = [{x: Ritare.mouseX, y: Ritare.mouseY}];
+		var scanned = [];
+		for(var x = 0; x < Ritare.canvas.width; x++){
+			scanned[x] = new Array(Ritare.canvas.width);
+		}
+		scanned[Ritare.mouseX][Ritare.mouseY] = true;
+		//get color of clicked pixel
+		var target = [];
+		var offset = (Ritare.mouseY * Ritare.canvas.width + Ritare.mouseX) * 4;
+		target[0] = canvas.data[offset];
+		target[1] = canvas.data[offset+1];
+		target[2] = canvas.data[offset+2];
+		//only fill if the selected color is different
+		if(!(target[0] == Ritare.colors[0] && target[1] == Ritare.colors[1] && target[2] == Ritare.colors[2])){
+			var translation = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+			for(var i = 0; i < scan.length; i++){
+				//fill the current pixel
+				var current = scan[i];
+				offset = (current.y * Ritare.canvas.width + current.x) * 4;
+				canvas.data[offset] = Ritare.colors[0];
+				canvas.data[offset+1] = Ritare.colors[1];
+				canvas.data[offset+2] = Ritare.colors[2];
+				//check adjacent pixels
+				for(var j = 0; j < 4; j++){
+					var k = {x: current.x + translation[j][0], y: current.y + translation[j][1]};
+					//don't go out of bounds
+					if(k.x >= 0 && k.x < Ritare.canvas.width && k.y >= 0 && k.y < Ritare.canvas.height){
+						offset = (k.y * Ritare.canvas.width + k.x) * 4;
+						//don't go over a pixel more than once
+						if(scanned[k.x][k.y] != true){
+							scanned[k.x][k.y] = true;
+							if(canvas.data[offset] == target[0] && canvas.data[offset+1] == target[1] && canvas.data[offset+2] == target[2]){
+								scan.push(k);
+							}
+							else{
+								//deal with anti-aliasing
+								canvas.data[offset] = Ritare.colors[0];
+								canvas.data[offset+1] = Ritare.colors[1];
+								canvas.data[offset+2] = Ritare.colors[2];
+							}
+						}
+					}
+				}
+			}
+		}
+		//write the stored copy back
+		Ritare.context.putImageData(canvas, 0, 0);
+	}
+	else if(Ritare.paint != true){
+		Ritare.paint = true;
+		Ritare.context.beginPath();
+		Ritare.context.fillStyle = Ritare.color;
+		Ritare.context.arc(Ritare.mouseX, Ritare.mouseY, Ritare.width/2, 0, 2*Math.PI)
+		Ritare.context.fill();
+	}
+}
+
+function move(e){
+	if(Ritare.paint){
+		if(e.type == 'mousemove' && Ritare.touch == false){
+			return;
+		}
+		Ritare.context.strokeStyle = Ritare.color;
+		Ritare.context.lineJoin = 'round';
+		Ritare.context.lineCap = 'round';
+		Ritare.context.lineWidth = Ritare.width;
+		Ritare.context.beginPath();
+		Ritare.context.moveTo(Ritare.mouseX, Ritare.mouseY)
+		offsets = getoffset(Ritare.canvas);
+		if(e.type == 'mousemove'){
+			Ritare.mouseX = e.pageX - offsets[0] - 2;
+			Ritare.mouseY = e.pageY - offsets[1] - 2;
+		}
+		else if(e.type == 'touchmove'){
+			Ritare.mouseX = e.changedTouches[0].pageX - offsets[0] - 2;
+			Ritare.mouseY = e.changedTouches[0].pageY - offsets[1] - 2;
+		}
+		Ritare.context.lineTo(Ritare.mouseX, Ritare.mouseY);
+		Ritare.context.stroke();
+	}
+}
+
+function end(e){
+	if(e.type == 'mousemove' && Ritare.touch == false){
+		return;
+	}
+	Ritare.paint = false;
+}
+
 var Ritare = {
 	parentel: null,
 	applet: null,
@@ -26,7 +133,8 @@ var Ritare = {
 	mouseX: null,
 	mouseY: null,
 	width: 3,
-	paint: null,
+	paint: false,
+	touch: null,
 	bucketfill: null,
 	start: function(options) {
 		this.parentel = document.getElementById(options.parentel);
@@ -99,89 +207,14 @@ var Ritare = {
 		this.applet.appendChild(this.finishbutton);
 		this.finishbutton.addEventListener('mousedown', options.onFinish);
 
-		this.canvas.addEventListener('mousedown', (function(e){
-			offsets = getoffset(Ritare.canvas);
-			Ritare.mouseX = e.pageX - offsets[0] - 2;
-			Ritare.mouseY = e.pageY - offsets[1] - 2;
-			if(Ritare.bucketfill){
-				//create a copy of the image's current state for modification
-				var canvas = Ritare.context.getImageData(0, 0, Ritare.canvas.width, Ritare.canvas.height);
-				//create an array of pixels to scan, and a 2d array to see if a pixel has already been scanned
-				var scan = [{x: Ritare.mouseX, y: Ritare.mouseY}];
-				var scanned = [];
-				for(var x = 0; x < Ritare.canvas.width; x++){
-					scanned[x] = new Array(Ritare.canvas.width);
-				}
-				scanned[Ritare.mouseX][Ritare.mouseY] = true;
-				//get color of clicked pixel
-				var target = [];
-				var offset = (Ritare.mouseY * Ritare.canvas.width + Ritare.mouseX) * 4;
-				target[0] = canvas.data[offset];
-				target[1] = canvas.data[offset+1];
-				target[2] = canvas.data[offset+2];
-				//only fill if the selected color is different
-				if(!(target[0] == Ritare.colors[0] && target[1] == Ritare.colors[1] && target[2] == Ritare.colors[2])){
-					var translation = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-					for(var i = 0; i < scan.length; i++){
-						//fill the current pixel
-						var current = scan[i];
-						offset = (current.y * Ritare.canvas.width + current.x) * 4;
-						canvas.data[offset] = Ritare.colors[0];
-						canvas.data[offset+1] = Ritare.colors[1];
-						canvas.data[offset+2] = Ritare.colors[2];
-						//check adjacent pixels
-						for(var j = 0; j < 4; j++){
-							var k = {x: current.x + translation[j][0], y: current.y + translation[j][1]};
-							//don't go out of bounds
-							if(k.x >= 0 && k.x < Ritare.canvas.width && k.y >= 0 && k.y < Ritare.canvas.height){
-								offset = (k.y * Ritare.canvas.width + k.x) * 4;
-								//don't go over a pixel more than once
-								if(scanned[k.x][k.y] != true){
-									scanned[k.x][k.y] = true;
-									if(canvas.data[offset] == target[0] && canvas.data[offset+1] == target[1] && canvas.data[offset+2] == target[2]){
-										scan.push(k);
-									}
-									else{
-										//deal with anti-aliasing
-										canvas.data[offset] = Ritare.colors[0];
-										canvas.data[offset+1] = Ritare.colors[1];
-										canvas.data[offset+2] = Ritare.colors[2];
-									}
-								}
-							}
-						}
-					}
-				}
-				//write the stored copy back
-				Ritare.context.putImageData(canvas, 0, 0);
-			}
-			else{
-				Ritare.paint = true;
-				Ritare.context.beginPath();
-				Ritare.context.fillStyle = Ritare.color;
-				Ritare.context.arc(Ritare.mouseX, Ritare.mouseY, Ritare.width/2, 0, 2*Math.PI)
-				Ritare.context.fill();
-			}
-		}));
+		this.canvas.addEventListener('mousedown', start);
+		this.canvas.addEventListener('touchstart', start);
+		
+		this.canvas.addEventListener('mousemove', move);
+		this.canvas.addEventListener('touchmove', move);
 
-		this.canvas.addEventListener('mousemove', (function(e){
-			if(Ritare.paint){
-				Ritare.context.strokeStyle = Ritare.color;
-				Ritare.context.lineJoin = 'round';
-				Ritare.context.lineCap = 'round';
-				Ritare.context.lineWidth = Ritare.width;
-				Ritare.context.beginPath();
-				Ritare.context.moveTo(Ritare.mouseX, Ritare.mouseY)
-				offsets = getoffset(Ritare.canvas);
-				Ritare.mouseX = e.pageX - offsets[0] - 2;
-				Ritare.mouseY = e.pageY - offsets[1] - 2;
-				Ritare.context.lineTo(Ritare.mouseX, Ritare.mouseY);
-				Ritare.context.stroke();
-			}
-		}));
-
-		document.body.addEventListener('mouseup', (function(e){
-			Ritare.paint = false;
-		}));
+		document.body.addEventListener('mouseup', end);
+		document.body.addEventListener('touchend', end);
+		
 	}
 };
